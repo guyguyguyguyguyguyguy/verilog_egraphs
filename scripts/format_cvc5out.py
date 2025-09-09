4bcc93d1cf3d3631f5bca0e57c3566895a48e2c6
guy <guy.h.frankel@gmail.com>
2025-09-09 16:44:10 +0100

    cleanup

--- scripts/format_cvc5out.py
+++
@@ -1,8 +0,0 @@
-def run(out_file):
-    with open('tmp.out', 'r') as f:
-        lines = f.readlines()
-
-    f_lines = [f"(\n{line.strip()}\n)" for line in lines if line.strip()]
-
-    with open(out_file, 'w') as f:
-        f.write('\n'.join(f_lines))
