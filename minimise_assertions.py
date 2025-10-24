6ccb112619982283c822affdeb39ba9ebc93568e
guy <guy.h.frankel@gmail.com>
2025-10-24 17:29:42 +0100

    parallelise chunks

--- minimise_assertions.py
+++ minimise_assertions.py
@@ -1,6 +1,10 @@
 import re
 import z3
+import time
 from itertools import combinations
+from more_itertools import chunked
+from multiprocessing import get_context, cpu_count
+import random
 
 
 def_pat = re.compile(r'.*define-fun.*')
@@ -13,6 +17,7 @@ class PowerLattice:
         self.decls = {}
         defined_funs, base = funs
         self.base = list(base)
+        random.shuffle(self.base)
         self.generate_vars(defined_funs)
 
         self._base = frozenset(range(len(self.base)))
@@ -25,6 +30,10 @@ class PowerLattice:
                         for b in self.base]
         self.neg_refs = [z3.Not(r) for r in self.base_refs]
         self.s = z3.Solver()
+
+    @staticmethod
+    def sort_by_complexity(base):
+        return list(sorted(base, key=lambda x: len(x.split(' '))))
 
     def generate_vars(self, exprs):
         for expr in exprs:
@@ -56,22 +65,31 @@ class PowerLattice:
             if next_can is None:
                 return
 
-            next_can_ass_idx = next_can                    
-            rst_idx = self._base - next_can                
+            next_can_ass_idx = next_can
+            rst_idx = self._base - next_can
 
             if self.test_can_refs(next_can_ass_idx, rst_idx):
                 self.curr_best = (next_can, level)
             else:
                 self.tried.add(next_can)
 
-
     def ensure_equal(self):
+        """Check both implications"""
         best_idx = self.curr_best[0]
-        best_refs = [self.base_refs[i] for i in best_idx]
-        self.s.push()
-        self.s.add(z3.And(self.base_refs) == z3.And(best_refs))
-        assert self.s.check() == z3.sat
-        self.s.pop()
+        removed_idx = self._base - best_idx
+
+        # This should always pass if minimization worked correctly
+        assert self.test_can_refs(best_idx, removed_idx), \
+            "Minimal doesn't imply removed constraints"
+
+        # Check reverse: can we satisfy base while violating minimal?
+        # If yes, we lost something
+        for kept_idx in best_idx:
+            test_remove = best_idx - {kept_idx}
+            if self.test_can_refs(test_remove, {kept_idx}):
+                print(f"ERROR: Constraint {kept_idx} is redundant but kept!")
+                assert False
+
 
     def minimise(self):
         self._minimise()
@@ -114,26 +132,40 @@ def get_unique_funs(defined_funs):
             pair[1].append(func)
     return pair
 
-def minimise(defined_funs, partition_size):
-    from more_itertools import chunked
+def _minimise_chunk(pairs):
+    pl = PowerLattice(zip(*pairs))
+    sub_min = pl.minimise()
+    return (len(pairs), sub_min)
 
+
+def minimise(defined_funs, partition_size, n_workers=None):
     partition_size = partition_size or len(defined_funs[0])
+    pairs_iter = zip(*defined_funs)
+    chunks = list(chunked(pairs_iter, partition_size))
 
     min_core = []
-    for p in chunked(zip(*defined_funs), partition_size):
-        pl = PowerLattice(zip(*p))
-        sub_min_core = pl.minimise()
-        print(f"sub before: {len(p)}, after: {len(sub_min_core)}")
-        min_core.extend(sub_min_core)
-        del pl
+
+    n_workers = min(n_workers or cpu_count(), len(chunks))
+
+    if n_workers <= 1 or len(chunks) == 1:
+        before, sub = _minimise_chunk(chunks[0])
+        print(f"sub before: {before}, after: {len(sub)}")
+        min_core.extend(sub)
+    else:
+        ctx = get_context("spawn") 
+        with ctx.Pool(processes=n_workers) as pool:
+            for before, sub in pool.map(_minimise_chunk, chunks):
+                print(f"sub before: {before}, after: {len(sub)}")
+                min_core.extend(sub)
+
     return min_core
 
-def run_minimisation(in_file, out_file, partition_size=None, ret=False):
+def run_minimisation(in_file, out_file, partition_size=None, ret=False, n_workers=None):
     defined_funs = get_defined_funs(in_file)
     unique_funs = get_unique_funs(defined_funs)
 
     print('start size: ', len(unique_funs[0]))
-    min_core = minimise(unique_funs, partition_size)
+    min_core = minimise(unique_funs, partition_size, n_workers=n_workers)
     print('final size: ', len(min_core))
 
     with open(out_file, 'w') as f:
@@ -142,23 +174,28 @@ def run_minimisation(in_file, out_file, partition_size=None, ret=False):
     if ret:
         return (len(unique_funs[0]), len(min_core))
 
-
 if __name__ == '__main__':
-    import sys
+    import sys, multiprocessing as mp
+    mp.set_start_method("spawn", force=True)
 
-    defaults = ["Sygus/s38417.sl", "tmp", None]
-    args = sys.argv[1:] + defaults[len(sys.argv)-1:]  # pad with defaults
+    defaults = ["Sygus/s1238.sl", "tmp", 2000, 9]
+    args = sys.argv[1:] + defaults[len(sys.argv)-1:]
 
     match sys.argv:
-        case [_]: 
-            file, out_file, partition_size = defaults
-        case [_, f]: 
-            file, out_file, partition_size = f, *defaults[1:]
-        case [_, f, o]: 
-            file, out_file, partition_size = f, o, defaults[2]
-        case [_, f, o, p]: 
-            file, out_file, partition_size = f, o, p
-        case _: 
+        case [_]:
+            file, out_file, partition_size, n_workers = defaults
+        case [_, f]:
+            file, out_file, partition_size, n_workers = f, *defaults[1:]
+        case [_, f, o]:
+            file, out_file, partition_size, n_workers = f, o, defaults[2], defaults[3]
+        case [_, f, o, p]:
+            file, out_file, partition_size, n_workers = f, o, p, defaults[3]
+        case [_, f, o, p, w]:
+            file, out_file, partition_size, n_workers = f, o, p, int(w) if w != "None" else None
+        case _:
             raise Exception("Too many arguments")
 
-    run_minimisation(file, out_file, partition_size)
+
+    start = time.time()
+    run_minimisation(file, out_file, partition_size, n_workers=n_workers)
+    print(time.time() - start)
