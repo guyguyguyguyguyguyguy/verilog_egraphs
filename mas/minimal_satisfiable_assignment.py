f4b132f4cc196a4edd4a178f1663e9d7be03896d
guy <guy.h.frankel@gmail.com>
2025-11-06 19:29:50 +0000

    making progress but wow its tough

--- mas/minimal_satisfiable_assignment.py
+++ mas/minimal_satisfiable_assignment.py
@@ -1,276 +1,252 @@
-# Taken from Mint -> https://alexeyignatiev.github.io/software/mintt ijk/
-# ==============================================================================
-from pysmt.smtlib.parser import SmtLibParser
-from pysmt.shortcuts import ForAll, get_model
 import re
-import time
-import random
-import z3
-import more_itertools
-from io import StringIO
-# ==============================================================================
+
+from collections import defaultdict
+from cvc5.pythonic import *
 
 
-class MistralMHSLattice:
-    """
-    Fixed version with correct MUS interpretation.
-    """
-    def __init__(self, simplify=True):
-        self.simplify = simplify
-        self.sname = "z3"
-        self.sat_calls = 0
-        self.known_sat = set()
-        self.known_unsat = set()
-        self.assertion_vars = None
-    
-    def solve(self, query, z3_vars=None, z3_solver=None):
-        self.script = SmtLibParser().get_script(StringIO(query))
-        self.formula = self.script.get_last_formula()
-        self.formula = self.formula.simplify()
-        self.fvars = self.formula.get_free_variables()
-        
-        print("Vars in assertions: ", len(self.fvars))
-        
-        self.known_sat = set()
-        self.known_unsat = set()
-        self.sat_calls = 0
-        self.assertion_vars = [sf.get_free_variables() for sf in self.formula.args()]
-        max_free_sets = self._search(self.assertion_vars)
-        print([len(x) for x in max_free_sets])
-        best_free_set = max(max_free_sets, key=lambda x: len(x))
-        return self.fvars - best_free_set
-    
-    def _compute_var_frequency(self, available_vars):
-        var_freq = {v: 0 for v in available_vars}
-        for assertion_var_set in self.assertion_vars:
-            for v in assertion_var_set:
-                if v in var_freq:
-                    var_freq[v] += 1
-        return var_freq
+seen_mus = []
+seen_notmus = []
+
+solver = Solver()
+solver.setOption("produce-proofs", "true") 
+solver.setOption("produce-models", "true") 
+
+def get_mus(v_file, a_file):
+    assertions, all_vars_inq, vars_per_assertion = get_assertions(a_file)   
+    variables = get_variables(v_file)
+
+    msas = search_msas(assertions, vars_per_assertion, all_vars_inq)
+    best_msa =min(msas, key=len)
+    amus = all_vars_inq - best_msa
+    best_msa = set(map(str, best_msa))
+
+    return variables - best_msa, best_msa,amus
+
+def search_msas(assertions, vars_per_assertion, all_vars, its=5):
+    mhs = approx_mhs(vars_per_assertion)
+    msas = []
+    for  _ in range(its):
+        match is_mus(all_vars - mhs, assertions):
+            case [True, _]: 
+                print(len(mhs))
+                # descend_to_boundary(mhs, query)
+                msas.append(mhs)
+            case [False, proof]:
+                print('before: ', len(mhs))
+                msa = ascend_to_boundary(mhs, vars_per_assertion,  all_vars, assertions, proof)
+                print('after: ', len(msa))
+                msas.append(msa)
+                include = (msa - mhs)
+                mhs = approx_mhs(vars_per_assertion, T=include)
+    return msas
+
+def most_frequent_var(vars_per_assertion):
+    counter = defaultdict(int)
+    for s in vars_per_assertion:
+        for v in s:
+            counter[v] +=1
+    return max(counter, key=counter.get) 
+
+def approx_mhs(vars_per_assertion,*, T=set()):
+    uncovered = {x for x in vars_per_assertion if not x & T}
+    T.add(most_frequent_var(uncovered))
+    while uncovered:
+        nv = most_frequent_var(uncovered)
+        T.add(nv)
+        uncovered = {x for x in uncovered if nv not in x} 
+    return T
+
+def is_mus(cand, assertions):
+    global seen_mus, seen_notmus
+
+    if any(cand.issubset(m) for m in seen_mus): 
+        return True
+    if any(n.issubset(cand) for n in seen_notmus):
+        return False
+
+    solver.push()
+    phi = ForAll(list(cand), assertions)
+    solver.add(phi)
+    if solver.check() == sat:
+        seen_mus = {m for m in seen_mus if not m.issubset(cand)}
+        seen_mus.add(frozenset(cand))
+        solver.pop()
+        return True, None
+    else:
+        sat_notmus = {n for n in seen_notmus if not cand.issubset(n)}
+        sat_notmus.add(frozenset(cand))
+        proof = solver.proof()
+        solver.pop()
+        return False, proof
+
+def ascend_to_boundary(mhs ,vars_per_assertion, all_vars, assertions, proof):
+    specified_vars = witnesses(all_vars - mhs, vars_per_assertion, assertions, proof)
+    msa = mhs.union(specified_vars)
+    assert is_mus(msa, assertions)
+    return msa
+
+def witnesses(cand, vpa, assertions, proof):
+    a_vars, b_vars = get_contra_vars(proof, cand, assertions)
+    A, B = set(), set()
+    for a, b in zip(a_vars, b_vars):
+        A.add(a)
+        cand_p = cand - A
+        if is_sat_forall(cand_p, assertions):
+            return cand - cand_p
+        B.add(b)
+        cand_p = cand - B
+        if is_sat_forall(cand_p, assertions):
+            return cand - cand_p
+    return set()
+
+def get_assertions(file):
+    func_pat = re.compile(r"(?:Bool|\(_\s+BitVec\s+\d+\))\s+(.*)\)")
+    var_pat = re.compile(r"\((\w+)\s+(?:(Bool)|\(_\s+(BitVec)\s+(\d+)\))\)")
+
+    assertions = set()
+    env = {}
+ 
+    with open(file, 'r') as f:
+        for l in f.readlines():
+            if (body := func_pat.search(l)) is not None:
+                expr = body.group(1)
+                for m in var_pat.findall(l):
+                    match m:
+                        case [n, "", _, w]:  env[n] = BitVec(n, int(w))
+                        case [n, _, "", ""]: env[n] = Bool(n)
+                assertions.add(parse_body_to_cvc5(expr, env=env))
+
+    assertions = And(*assertions)
+    free_vars = free_variables(assertions)
+    return assertions, frozenset(free_vars), [frozenset(free_variables(a)) for a in assertions.children()]
 
 
-    # TODO: Need a way to get the (minimal) unsat core
-    def descend_to_boundary(self, X_free):
-        """Use PySMT's UnsatCoreSolver with proper tracking."""
-        from pysmt.shortcuts import Solver, Not, ForAll, Implies, And
-        
-        X_list = list(X_free)
-        
-        if not X_list:
-            return frozenset()
-        
-        if self.get_model_forall_lattice(X_free):
-            return frozenset(X_free)
-        
-        # Strategy: For each conjunction in the formula, track which variables appear
-        # Then find which conjunctions are in the UNSAT core
-        
-        with Solver(name=self.sname, unsat_cores_mode='named') as solver:
-            # Get individual conjuncts from the formula
-            if self.formula.is_and():
-                conjuncts = self.formula.args()
-            else:
-                conjuncts = [self.formula]
-            
-            # Add each conjunct as a named assertion with quantification
-            for i, conjunct in enumerate(conjuncts):
-                # Only quantify over variables that appear in both X_free and this conjunct
-                conjunct_vars = conjunct.get_free_variables()
-                vars_to_quantify = X_free.intersection(conjunct_vars)
-                
-                if vars_to_quantify:
-                    assertion = ForAll(vars_to_quantify, conjunct)
-                    assertion_name = f"conjunct_{i}"
-                    solver.add_assertion(assertion, named=assertion_name)
-            
-            result = solver.solve()
-            
-            if not result:  # UNSAT - we have a conflict!
-                core = solver.get_named_unsat_core()
-                
-                # Find which variables are in the core conjuncts
-                conflicting_vars = set()
-                for name in core.keys():
-                    if name.startswith('conjunct_'):
-                        idx = int(name.replace('conjunct_', ''))
-                        if idx < len(conjuncts):
-                            conjunct_vars = conjuncts[idx].get_free_variables()
-                            conflicting_vars.update(X_free.intersection(conjunct_vars))
-                
-                conflicting_vars = frozenset(conflicting_vars)
-                
-                if conflicting_vars:
-                    X_sat = X_free - conflicting_vars
-                    
-                    if not X_sat or not self.get_model_forall_lattice(X_sat):
-                        if X_sat:
-                            return self.descend_to_boundary(X_sat)
-                        else:
-                            return frozenset()
-                    
-                    # Try to add back variables
-                    for var in conflicting_vars:
-                        X_candidate = X_sat.union(frozenset([var]))
-                        if self.get_model_forall_lattice(X_candidate):
-                            X_sat = X_candidate
-                    
-                    return frozenset(X_sat)
-        
-        return self._descend_systematic(X_free)
+# Helpers 
 
-    def _descend_systematic(self, X_free):
-        """Fallback: systematic removal."""
-        X_list = list(X_free)
-        
-        if not X_list:
-            return frozenset()
-        
-        var_freq = self._compute_var_frequency(X_list)
-        X_list.sort(key=lambda v: var_freq[v], reverse=True)
-        
-        X_current = X_free
-        
-        for var in X_list:
-            X_without = X_current - frozenset([var])
-            
-            if self.get_model_forall_lattice(X_without):
-                # Try to add back vars
-                for v in X_list:
-                    if v not in X_without:
-                        X_cand = X_without.union(frozenset([v]))
-                        if self.get_model_forall_lattice(X_cand):
-                            X_without = X_cand
-                return frozenset(X_without)
-            
-            X_current = X_without
-        
-        return frozenset()
-    
-    def _search(self, assertion_vars):
-        mhss = [self.get_mhs(assertion_vars)]
-        free_sets = []
-        for _ in range(5):
-            mhs = mhss.pop()
-            print(len(mhs), len(self.assertion_vars))
-            if self.get_model_forall_lattice(mhs):
-                free_sets.append(mhs)
-                return free_sets
-            else:
-                free_set = self.descend_to_boundary(mhs)
-                free_sets.append(free_set)
-                new_mhs = self.get_mhs(assertion_vars, mhs - free_set)
-                mhss.append(new_mhs)
-        return free_sets
-    
-    def get_mhs(self, assertion_vars, to_ignore = set()):
-        uncovered_sets = [frozenset(var_set) for var_set in assertion_vars]
-        mhs = set()
-        
-        while uncovered_sets:
-            var_freq = {}
-            for var_set in uncovered_sets:
-                for var in var_set:
-                    if var not in mhs and var not in to_ignore:
-                        var_freq[var] = var_freq.get(var, 0) + 1
-            
-            if not var_freq:
-                break
-            
-            best_var = max(var_freq.keys(), key=lambda v: var_freq[v])
-            mhs.add(best_var)
-            
-            uncovered_sets = [s for s in uncovered_sets if best_var not in s]
-        
-        return mhs
-    
-    def get_model_forall_lattice(self, x_univl):
-        x_set = frozenset(x_univl)
-        
-        if any(unsat_set.issubset(x_set) for unsat_set in self.known_unsat):
-            return False
-        
-        if any(x_set.issubset(sat_set) for sat_set in self.known_sat):
-            return True
-        
-        self.sat_calls += 1
-        result = get_model(ForAll(x_univl, self.formula), solver_name=self.sname)
+def _tok(s: str):
+    s = s.replace('(', ' ( ').replace(')', ' ) ')
+    return [t for t in s.split() if t]
 
-        if result is not None:
-            self.known_sat = {s for s in self.known_sat if not s.issubset(x_set)}
-            self.known_sat.add(x_set)
+def _parse(tokens):
+    t = tokens.pop(0)
+    if t == '(':
+        lst = []
+        while tokens[0] != ')':
+            lst.append(_parse(tokens))
+        tokens.pop(0)
+        return lst
+    elif t == ')':
+        raise ValueError('unmatched )')
+    else:
+        return t
+
+def _eval(ast, env, bv_widths):
+    if isinstance(ast, str):
+        if ast == 'true':  return BoolVal(True)
+        if ast == 'false': return BoolVal(False)
+        # use declared symbol if present; otherwise assume Bool
+        if ast in env:
+            return env[ast]
+        v = Bool(ast)
+        env[ast] = v
+        return v
+
+    if isinstance(ast, list) and len(ast) == 3 and ast[0] == '_' and str(ast[1]).startswith('bv'):
+        val = int(str(ast[1])[2:]); width = int(ast[2])
+        return BitVecVal(val, width)
+
+    op, *args = ast
+    if op == 'and':  return And(*[_eval(a, env, bv_widths) for a in args])
+    if op == 'or':   return Or(*[_eval(a, env, bv_widths) for a in args])
+    if op == 'not':  return Not(_eval(args[0], env, bv_widths))
+    if op == 'xor':  return Xor(_eval(args[0], env, bv_widths), _eval(args[1], env, bv_widths))
+    if op == '=>':   return Implies(_eval(args[0], env, bv_widths), _eval(args[1], env, bv_widths))
+    if op == 'ite':
+        c,a,b = (_eval(x, env, bv_widths) for x in args)
+        return If(c,a,b)
+    if op == '=':
+        a = _eval(args[0], env, bv_widths); b = _eval(args[1], env, bv_widths)
+        return a == b
+    # simple BV ops (extend as needed)
+    if op == 'bvnot': return ~_eval(args[0], env, bv_widths)
+    if op == 'bvand': return _eval(args[0], env, bv_widths) & _eval(args[1], env, bv_widths)
+    if op == 'bvor':  return _eval(args[0], env, bv_widths) | _eval(args[1], env, bv_widths)
+    if op == 'bvxor': return _eval(args[0], env, bv_widths) ^ _eval(args[1], env, bv_widths)
+    if op == 'bvadd': return _eval(args[0], env, bv_widths) + _eval(args[1], env, bv_widths)
+    if op == 'bvsub': return _eval(args[0], env, bv_widths) - _eval(args[1], env, bv_widths)
+    if op == 'bvmul': return _eval(args[0], env, bv_widths) * _eval(args[1], env, bv_widths)
+    if op == 'bvneg': return -_eval(args[0], env, bv_widths)
+    if op == 'concat': return Concat(*[_eval(a, env, bv_widths) for a in args])
+    if op == 'bvult':
+        a,b = _eval(args[0], env, bv_widths), _eval(args[1], env, bv_widths)
+        return ULT(a,b)
+    if op == 'bvule':
+        a,b = _eval(args[0], env, bv_widths), _eval(args[1], env, bv_widths)
+        return ULE(a,b)
+    if op == 'bvugt':
+        a,b = _eval(args[0], env, bv_widths), _eval(args[1], env, bv_widths)
+        return UGT(a,b)
+    if op == 'bvuge':
+        a,b = _eval(args[0], env, bv_widths), _eval(args[1], env, bv_widths)
+        return UGE(a,b)
+
+    # unknown operator -> treat as symbol (rare in bodies)
+    return env[op]
+
+def parse_body_to_cvc5(body: str, *, env, bv_widths=None):
+    tokens = _tok(body)
+    ast = _parse(tokens)
+    return _eval(ast, env, bv_widths or {})
+
+def get_contradictions(p):
+    out = []
+    stack = [p]
+    while stack:
+        node = stack.pop()
+        if node.getRule().name == "CONTRA":
+            out.append(node)
+        stack.extend(node.getChildren())
+    return out
+
+def get_contra_vars(p, cand, assertions):
+    cons = get_contradictions(p)
+    ass = assertions.children()
+    con ,= cons
+    r_vars = []
+    c1, c2 =  con.getChildren()
+    c1idx, c2idx = c1.getArguments()[0].as_long(), c2.getArguments()[0].as_long()
+    ra = [ass[c1idx], ass[c2idx]]
+    for a in ra:
+        vs = set()
+        vs = free_variables(a)
+        r_vars.append(cand & vs)
+    return r_vars
+
+def is_sat_forall(vars_set, query):
+    solver.push()
+    solver.add(ForAll(list(vars_set), query))
+    r = solver.check()
+    solver.pop()
+    return r == sat
+
+def free_variables(a):
+    vs = set()
+    stack = [a]
+    while stack:
+        t = stack.pop()
+        if (cs := t.children()):
+            stack.extend(cs)
         else:
-            self.known_unsat = {s for s in self.known_unsat if not x_set.issubset(s)}
-            self.known_unsat.add(x_set)
-
-        return result is not None
-
-# Regex definitions
-def_pat = re.compile(r".*define-fun.*")
-func_pat = re.compile(r"(?:Bool|\(_\s+BitVec\s+\d+\))\s+(.*)\)")
-var_pat = re.compile(r"\((\w+)\s+(?:(Bool)|\(_\s+(BitVec)\s+(\d+)\))\)")
-
-def get_defined_funs(file):
-    with open(file, "r") as f:
-        content = f.read()
-        matches = def_pat.findall(content)
-    return set(matches)
-
-def generate_vars(expr, d):
-    for m in var_pat.findall(expr):
-        match m:
-            case [n, "", _, w]:
-                tmp = z3.BitVec(n, int(w))
-            case [n, _, "", ""]:
-                tmp = z3.Bool(n)
-        d[n] = tmp
-
-def gen_vars_get_fun(expr, d):
-    generate_vars(expr, d)
-    return func_pat.search(expr).group(1)
-
-def get_mas(exprs, d, m):
-    assertion = f"""
-    (assert
-        (and {(' ').join(exprs)})
-    )
-    """
-    z3_assertion = z3.parse_smt2_string(assertion, decls=d)
-    s = z3.Solver()
-    s.add(z3_assertion)
-    mas = m.solve(s.to_smt2())
-    return mas
+            vs.add(t)
+    return vs
 
 def get_variables(file):
     with open(file, "r") as f:
         variables = set(v for v in f.read().split())
     return variables
 
-def run_get_mas(vf="../Variables/s38584.txt", sf="../Sygus/s38584.sl"):
-    import time
-    start = time.time()
-    variables = get_variables(vf)
-    matches = get_defined_funs(sf)
 
-    m = MistralMHSLattice()
-    d = {}
-    assertions = {gen_vars_get_fun(a, d) for a in matches}
-    print("vars: ", len(variables))
-    mas = get_mas(assertions, d, m)
-    print("MUS: ", len(m.fvars - mas))
-    mas = {str(a) for a in mas}
-    underspecified = variables - mas
-    print("underspecified: ", len(underspecified))
-    print("time: ", time.time() - start)
-    return underspecified
-
-import sys
+get_mus("../Variables/c17.txt", '../Sygus/c17.sl')
 
 
-if __name__ == '__main__':
-    print(sys.argv)
-    if len(sys.argv) == 2:
-        vf = sys.argv[1]
-        sf = f"../Sygus/{re.search(r'Variables\/(.*)\.txt', vf).group(1)}.sl"
-        _ = run_get_mas(vf, sf)
-    else:
-        _ = run_get_mas()
+
